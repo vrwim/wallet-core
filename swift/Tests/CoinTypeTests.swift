ceabe3c59f103743723f4cfec863e832120e1e1a
Wim Van Renterghem <git@vrwim.com>
2021-11-19 15:33:41 +0100

    Swift & Android tests

--- swift/Tests/CoinTypeTests.swift
+++ swift/Tests/CoinTypeTests.swift
@@ -29,5 +29,6 @@ class CoinTypeTests: XCTestCase {
         XCTAssertEqual(CoinType.nebulas.rawValue, 2718)
         XCTAssertEqual(CoinType.avalancheCChain.rawValue, 10009000)
         XCTAssertEqual(CoinType.xdai.rawValue, 10000100)
+        XCTAssertEqual(CoinType.energyWebChain.rawValue, 10000100)
     }
 }
